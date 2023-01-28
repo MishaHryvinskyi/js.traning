ce2d8d9b37bdbec65bdeff247e0d0b432268f092
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-01-28 13:13:39 +0200

    і

--- Js/modal.js
+++ Js/modal.js
@@ -1,11 +1,20 @@
 
 
-const a = 10;
-const b = 3;
-const c = a + b;
-const g = b - a;
+// const a = 10;
+// const b = 3;
+// const c = a + b;
+// const g = b - a;
 
-// console.log(g);
-// console.log(c);
-// console.log(a + b);
-// console.log(a % b);
+// // console.log(g);
+// // console.log(c);
+// // console.log(a + b);
+// const type = typeof b;
+// console.log(type);
+// console.log('Кчічка смерічка',);
+
+const type = typeof 'Кчічка';
+console.log(type);
+
+console.log('мЕ');
+alert('КЧІЧКА');
+console.log('Не мекай!');
