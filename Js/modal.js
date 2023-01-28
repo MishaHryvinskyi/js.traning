0943d8a4d4df642b68f7a0b151b7f13b820b5c4a
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-01-28 19:57:47 +0200

    па

--- Js/modal.js
+++ Js/modal.js
@@ -1,4 +1,15 @@
 
 
-const shouldRenew = confirm ('А бехі будуть?');
-console.log(confirm);
+// let elementWidth = '50px';
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
+
+
+// 
+
+let salary = 1300.16472;
+
+salary = salary.toFixed(2);
+salary = Number(salary);
+
+console.log(salary);
