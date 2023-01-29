3b02200d023a15a6003e7a4acfa5a7a9a568f6aa
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-01-29 20:14:13 +0200

    чя

--- Js/new.js
+++ Js/new.js
@@ -6,6 +6,15 @@
 // let brand = prompt('Давай брнд');
 // brand = brand.toLowerCase();
 // console.log(brand);
-let brand = 'samSUnG';
-console.log(brand[4]);
-console.log(brand.slice(3).toLocaleLowerCase());
+const blackListedWord1 = 'спам';
+const blackListedWord2 = 'розпродаж';
+const string1 = 'Привіт, я принц Абдул, це не спам, пропоную тобі міліон';
+const string2 = 'Самий найбільший РОЗПРОДАЖ цієї неділі, непропусть';
+const string3 = 'Рекламна компанія #fatlivesmatter';
+console.log(string1.includes(blackListedWord1));
+console.log(string1.includes(blackListedWord2));
+console.log(string2.includes(blackListedWord1));
+const normalizedString2 = string2.toLowerCase();
+console.log(normalizedString2.includes(blackListedWord2));
+console.log(string3.includes(blackListedWord1));
+console.log(string3.includes(blackListedWord2));
