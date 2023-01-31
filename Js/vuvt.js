a5a24627ece773cf6882a5b5245c66d9d6ce97fd
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-01-31 23:23:16 +0200

    d

--- Js/vuvt.js
+++ Js/vuvt.js
@@ -1,50 +1,22 @@
-// const salary = 100;
 
-// if(salary <= 500){
-//     console.log('Рівень1');
-// }else if(salary > 500 && salary < 1500){
-//     console.log('Рівень2');
-// }else if(salary > 1500 && salary < 3000){
-//     console.log('Рівень3');
-// }else {
-//     console.log('Рівень4');
-// }
+// Напишіть скрипт який підрахує суму всіх парних чисел в масиві
+const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
+let total = 0;
+// 1 Перемінна total
 
-// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
-// console.table(friends);
-// console.table(friends[0]);
 
-// const lastIndex = friends.length -1;
-// console.log(lastIndex);
-// friends[1] = 'Vuvt';
-// friends[3] = 71;
-// console.table(friends);
 
-// let a = 10;
-// let b = a;
-// console.log(a);
-// console.log(b);
-// a = 20;
-// console.log(a);
-// console.log(b);
+// 2 Перебрати масив
+for (let i = 0; i < numbers.length; i += 1) {
+console.log(numbers[i]);
+}
 
-const a = [];
-console.log(a);
-a.push(1);
-console.log(a);
+// 3 На кожній ітерації перевірити елемент на парність
+if(numbers[i] % 2 === 0) {
+    console.log('Парні!!!');
 
-a.push(2);
-console.log(a);
 
-a.push(3);
-console.log(a);
-
-console.log(a.pop());
-console.log(a);
-
-console.log(a.pop());
-console.log(a);
-
-console.log(a.pop());
-console.log(a);
-
+// 4 Якщо парне, плюсуємо до total
+    total += numbers[i];
+}
+console.log('Total:', total);
