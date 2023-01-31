f2515b315fa7598748a99fc3160839985356235c
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-01-31 18:49:12 +0200

    р

--- Js/vuvt.js
+++ Js/vuvt.js
@@ -1,11 +1,50 @@
-const salary = 100;
+// const salary = 100;
 
-if(salary <= 500){
-    console.log('Рівень1');
-}else if(salary > 500 && salary < 1500){
-    console.log('Рівень2');
-}else if(salary > 1500 && salary < 3000){
-    console.log('Рівень3');
-}else {
-    console.log('Рівень4');
-}
+// if(salary <= 500){
+//     console.log('Рівень1');
+// }else if(salary > 500 && salary < 1500){
+//     console.log('Рівень2');
+// }else if(salary > 1500 && salary < 3000){
+//     console.log('Рівень3');
+// }else {
+//     console.log('Рівень4');
+// }
+
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// console.table(friends);
+// console.table(friends[0]);
+
+// const lastIndex = friends.length -1;
+// console.log(lastIndex);
+// friends[1] = 'Vuvt';
+// friends[3] = 71;
+// console.table(friends);
+
+// let a = 10;
+// let b = a;
+// console.log(a);
+// console.log(b);
+// a = 20;
+// console.log(a);
+// console.log(b);
+
+const a = [];
+console.log(a);
+a.push(1);
+console.log(a);
+
+a.push(2);
+console.log(a);
+
+a.push(3);
+console.log(a);
+
+console.log(a.pop());
+console.log(a);
+
+console.log(a.pop());
+console.log(a);
+
+console.log(a.pop());
+console.log(a);
+
