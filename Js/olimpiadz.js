1f10df1606f7aa971125a91c8abdfdc03c6f1718
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-02-19 13:28:49 +0200

    р

--- Js/olimpiadz.js
+++ Js/olimpiadz.js
@@ -241,29 +241,40 @@
 //   return x % y;
 // });
 
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName, onSuccess, onError) {
-    if (this.pizzas.includes(pizzaName)) {
-      return makePizza(pizzaName);
-  }
-    return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
-  },
-};
-// Change code above this line
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return makePizza(pizzaName);
+//   }
+//     return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+//   },
+// };
+// // Change code above this line
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+
+
+const buttonRef = document.querySelector('.js-button');
+
+const handleButtonClick = function (){
+  console.log('Клік по кнопці' + Date.now());
 }
 
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
+buttonRef.addEventListener('click', handleButtonClick);
 
-// Method calls with callbacks
-pizzaPalace.order("Smoked", makePizza, onOrderError);
-pizzaPalace.order("Four meats", makePizza, onOrderError);
-pizzaPalace.order("Big Mike", makePizza, onOrderError);
-pizzaPalace.order("Vienna", makePizza, onOrderError);
+window.navigator.geolocation.getCurrentPosition();
