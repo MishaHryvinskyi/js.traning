6fe8255b1827f9328ddf97ab68515ea1f2d90a3a
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-03-13 10:53:29 +0200

    в

--- Js/modal.js
+++ Js/modal.js
@@ -1,9 +1,16 @@
 
-const colors = ['tomato', 'teal', 'orange', 'skyblue'];
-const max = colors.length - 1;
-const min = 0;
-const index = Math.round(Math.random() * (max - min) + min);
-const color = colors [index];
-console.log(color);
-document.body.style.backgroundColor = color;
+// const colors = ['tomato', 'teal', 'orange', 'skyblue'];
+// const max = colors.length - 1;
+// const min = 0;
+// const index = Math.round(Math.random() * (max - min) + min);
+// const color = colors [index];
+// console.log(color);
+// document.body.style.backgroundColor = color;
 
+// const magiscBtn = document.querySelector('.btn-chengese');
+// magicBtn.addEventListener ('click', () => {
+//     const imgEl = document.querySelector('.img_user');
+//     console.log('navEl', imgEl);
+//     console.log(imgEl.src);
+//     imgEl.src = "http://127.0.0.1:5500/images/katia.jpg";
+// }) ;
